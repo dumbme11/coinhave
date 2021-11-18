cc544242967430be28c1af752196b819b56426e7
ertovkax <s280906k@gmail.com>
2021-11-18 09:33:41 +0300

    Updates

---
+++ precache-manifest.03513e8fc8e835e3c34a7753dd35ffab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1b987b07e2f0bea4d5197322a17329",
+    "url": "/cpcpcin/index.html"
+  },
+  {
+    "revision": "41017f31e45096952f33",
+    "url": "/cpcpcin/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "/cpcpcin/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "41017f31e45096952f33",
+    "url": "/cpcpcin/static/js/2.3e81fa50.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/cpcpcin/static/js/2.3e81fa50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "/cpcpcin/static/js/main.c9fee35b.chunk.js"
+  },
+  {
+    "revision": "4d396b5db6f6a23de694",
+    "url": "/cpcpcin/static/js/runtime-main.d2be6aee.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/cpcpcin/static/media/persik.4e1ec840.png"
+  }
+]);
